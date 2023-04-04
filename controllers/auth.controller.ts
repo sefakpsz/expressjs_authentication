4e861b1e26b68122448d019a6d49f53bbac58d81
Sefa Kapısız <110828141+sefakpsz14@users.noreply.github.com>
2023-04-05 01:22:41 +0300

    auth controller infrastructure

---
+++ controllers/auth.controller.ts
@@ -0,0 +1,19 @@
+export const login = () => {
+
+}
+
+export const logout = () => {
+
+}
+
+export const register = () => {
+
+}
+
+export const passwordChange = () => {
+
+}
+
+export const passwordReset = () => {
+
+}
