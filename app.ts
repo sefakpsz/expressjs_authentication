86fec13cc2f0a9f684f319cbc6ee7b2864871104
sefa kapısız <sefa.kapisiz@coino.com>
2023-04-10 21:51:38 +0300

    dotenv was added

--- app.ts
+++ app.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 import mongoose from 'mongoose';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import auth from './routes/auth.routes'
 
 const app = express();
@@ -11,7 +15,7 @@ app.use('/auth', auth);
 app.listen(1907, async () => {
     await mongoose.connect("mongodb+srv://sefakpsz:paOy6AywARUAsOS1@cluster0.y4mw8zp.mongodb.net/BaseDb?retryWrites=true&w=majority")
         .then(() => {
-            console.log("App is started successfully!\nConnected to MongoDb!")
+            console.log(`Connected to MongoDb!\nhttp://127.0.0.1:${process.env.PORT} is listening...`)
         })
         .catch((error) => {
             console.log(error)
