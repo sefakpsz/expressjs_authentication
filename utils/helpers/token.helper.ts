24f0942d309b3a40bbcaaffcfde28945c3431398
coino <sefa.kapisiz@coino.com>
2023-04-23 19:19:33 +0300

    progression

---
+++ utils/helpers/token.helper.ts
@@ -0,0 +1,6 @@
+const createToken = (email: string, name: string, surname: string) => {
+    /*
+    use a hash function in here and use provided strings as hash and remember to salt them with 'hashFunction'.key()
+    emailMSKnameMSKsurname,Date.now()
+    */
+}
